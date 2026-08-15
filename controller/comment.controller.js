dc3cf38480abd74a07a62d6366fd450b5e02bad7
annan john <155844175+annan-7@users.noreply.github.com>
2026-08-14 23:00:42 -0400

    Adding comment controller

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -5,6 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.models.js";
 import { Mongoose } from "mongoose";
 import { Video } from "../models/video.models.js";
+import { VideoQuality } from "../models/video-quality.models.js";
 
 const createComment = asyncHandler(async(req, res)=>{
     const {content} = req.body;
@@ -14,7 +15,8 @@ const createComment = asyncHandler(async(req, res)=>{
     if(!content){
         throw new ApiError(400, "Please enter your comment")
     };
-    const videoExist = await Video.findById(video);
+    const videoExist = await VideoQuality.findById(video)
+    console.log(videoExist)
 
     if(!videoExist){
         throw new ApiError(404, "Video not found or has been deleted")
@@ -50,7 +52,38 @@ const getComments = asyncHandler(async(req, res)=>{
         new ApiResponse(200, "Comments fetched successfully", comments)
     )
 })
+const deleteComment = asyncHandler(async(req,res )=>{
+    const {commentId}= req.params;
+
+    if(!commentId){
+        return new ApiResponse(400, " Comment Id is required")
+    }
+    
+    const DeleteComment = await Comment.findByIdAndDelete(commentId)
+
+    if(!DeleteComment){
+        return new ApiResponse(404, "Comment not found or has been deleted")
+    }
+     
+    return res.status(200).json(
+        new ApiResponse(200, {},"Comment has been deleted successfully")
+    )
+})
+
+const ChangeComment = asyncHandler(async(req,res)=>{
+    const {commentID} = req.params;
+    const {content} = req.body;
+
+    if(!commentID){
+        throw new ApiError(400,'Comment ID is not available or has been deleted')
+    }
+    if(!content){
+        throw new ApiError(400, "Please enter your comment")
+    }
+})
 export {
     createComment,
-    getComments
+    getComments,
+    deleteComment
+
 }
